946c8706ca6dc3c0349221f457ef9eae8f9b515e
Niladri Tewari <142900410+Niladri11@users.noreply.github.com>
2026-03-04 10:57:38 +0530

    Set up AWS infrastructure for Terraform state
    Configured AWS provider, S3 bucket for remote state, versioning, encryption, and DynamoDB table for state locking.

---
+++ bootstrap/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# S3 bucket for remote state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-demo-testing-2026-niladri"
+}
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
